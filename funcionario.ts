d26351e9e68ad0cf0f51eb43640205c7848e598a
Flips69 <aion.lipe@gmail.com>
2023-08-29 12:09:11 -0300

    Exercicio 4 - Funcionários

---
+++ funcionario.ts
@@ -0,0 +1,54 @@
+class Funcionario{
+    nome: string;
+    cargo: string;
+    salario: number; 
+    inss: number = 11;
+    ir: number = 7.5;
+
+    constructor(nome: string, cargo: string, salario: number, inss: number, ir: number){
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario =  salario;
+        this.inss = inss;
+        this.ir = ir;
+
+    }
+    Apresentacao(){
+        return "Olá " + this.nome + ", seu salário bruto é de: R$" + this.salario + ".";
+    }
+
+    totalImpostos(){
+        let inss = ((this.inss * this.salario)/100)
+        let liquido = (this.salario - inss)
+        let ir = ((this.ir * liquido)/100)
+        let total = (inss + ir)
+        return total;
+    }
+
+    aplicarAumento(percentual: number){
+        let aumento = (this.salario/100)*percentual;
+        return this.salario + aumento;
+        //return "Aumento de " + percentual + "% aplicado. Novo salário" + "R$: " + this.salario.toFixed(2);
+    }
+
+    aumentoAplicado(percentual: number){
+        let aumento = (this.salario/100)*percentual;
+        let salario = this.salario + aumento;
+        return "Aumento de " + percentual + "% aplicado. Novo salário de: R$" +salario.toFixed(2);
+        
+    }
+
+    exibirDetalheFuncionario(){
+        let total = this.totalImpostos();
+        return "Funcionário: " + this.nome + ", Cargo: " + this.cargo + ", Salário Bruto de: R$" + this.salario + ", Imposto R$" + total + ", Salário Líquido: R$" + (this.salario - total);
+    }
+
+    
+}
+
+let funcionario = new Funcionario("Ana Silva", "Analista de Vendas", 3000, 11, 7.5);
+console.log(funcionario.Apresentacao());
+console.log(funcionario.totalImpostos());
+console.log(funcionario.aplicarAumento(10));
+console.log(funcionario.aumentoAplicado(10));
+console.log(funcionario.exibirDetalheFuncionario());
